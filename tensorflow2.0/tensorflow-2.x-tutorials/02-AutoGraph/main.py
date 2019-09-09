6f5093200456c535ae96a699a02b5e32ccbbfcda
jobsjiang <jht727415@163.com>
2019-09-09 16:40:15 +0800

    autograph

--- tensorflow2.0/tensorflow-2.x-tutorials/02-AutoGraph/main.py
+++ tensorflow2.0/tensorflow-2.x-tutorials/02-AutoGraph/main.py
@@ -13,3 +13,14 @@ def fn(input,state):
     :return:
     """
     return cell(input,state)
+
+input = tf.zeros([10,10])
+state = [tf.zeros([10,10])] * 2
+
+cell(input,state)
+fn(input,state)
+
+dynamic_graph_time = timeit.timeit(lambda :cell(input,state),number=100)
+static_graph_time = timeit.timeit(lambda :fn(input,state),number=100)
+print('dynamic_graph_time:',dynamic_graph_time)
+print('static_graph_time:',static_graph_time)
