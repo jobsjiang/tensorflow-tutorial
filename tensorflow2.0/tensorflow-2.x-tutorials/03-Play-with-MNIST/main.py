ef34daadb809fdaa7c3504185f2b15ef8d4c7d11
jobsjiang <jht727415@163.com>
2019-09-11 14:12:46 +0800

    mnist

--- tensorflow2.0/tensorflow-2.x-tutorials/03-Play-with-MNIST/main.py
+++ tensorflow2.0/tensorflow-2.x-tutorials/03-Play-with-MNIST/main.py
@@ -4,4 +4,17 @@ from tensorflow.keras import datasets,layers,optimizers,Sequential,metrics
 (xs,ys,),_ = datasets.mnist.load_data()
 print('datasets:',xs.shape,ys.shape,xs.min(),xs.max())
 xs = tf.convert_to_tensor(xs,dtype=tf.float32) / 255
+db = tf.data.Dataset.from_tensor_slices((xs,ys))
+db = db.batch(32).repeat(10)
+# 模型网络
+network = Sequential([
+    layers.Dense(256,activation='relu'),
+    layers.Dense(256,activation='relu'),
+    layers.Dense(256,activation='relu'),
+    layers.Dense(10)
+])
+network.build(input_shape=(None,28*28))
+network.summary()
 
+optimizer = optimizers.SGD(lr=0.01)
+acc_meter = metrics.Accuracy()
