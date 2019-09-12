e7421daf49b52383510a01eb34e72d21aa23fc85
jobsjiang <jht727415@163.com>
2019-09-12 13:02:13 +0800

    mnist

--- tensorflow2.0/tensorflow-2.x-tutorials/03-Play-with-MNIST/main.py
+++ tensorflow2.0/tensorflow-2.x-tutorials/03-Play-with-MNIST/main.py
@@ -18,3 +18,24 @@ network.summary()
 
 optimizer = optimizers.SGD(lr=0.01)
 acc_meter = metrics.Accuracy()
+
+for step,(x,y) in enumerate(db):
+    with tf.GradientTape() as tape:
+        # [b,28,28] => [b,784]
+        x = tf.reshape(x,(-1,28*28))
+        # [b,784] => [b,10]
+        out = network(x)
+        # [b] => [b,10]
+        y_onehot = tf.one_hot(y,depth=10)
+        # [b,10]
+        loss = tf.square(out - y_onehot)
+        # [b] 32代表batch
+        loss = tf.reduce_mean(loss) / 32
+    acc_meter.update_state(tf.argmax(out,axis=1),y)
+
+    grads = tape.gradient(loss,network.trainable_variables)
+    optimizer.apply_gradients(zip(grads,network.trainable_variables))
+
+    if step % 200 == 0:
+        print(step,'loss:',float(loss),'acc:',acc_meter.result().numpy())
+        acc_meter.reset_states()
