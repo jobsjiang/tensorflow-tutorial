7d48e3bd8204fbed79479a8e4124d9e2a3861653
jobsjiang <jht727415@163.com>
2019-09-16 12:52:14 +0800

    LR

--- tensorflow2.0/tensorflow-2.x-tutorials/04-Linear-Regression/main.py
+++ tensorflow2.0/tensorflow-2.x-tutorials/04-Linear-Regression/main.py
@@ -1,1 +1,12 @@
 # -*- coding：utf-8 -*-
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+import os
+class Regressor(keras.layers.Layer):
+    def __init__(self):
+        super(Regressor,self).__init__()
+        # [dim_in,dim_out]
+        self.w = self.add_variable('meanless-name',[13,1])
+        # [dim_out]
+        self.b = self.add_variable('meanless-name',[1])
