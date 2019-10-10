1d454479a4827695e4fff1595c5fdc130a34be20
jobsjiang <jht727415@163.com>
2019-10-10 18:44:40 +0800

    lr

--- tensorflow2.0/tensorflow-2.x-tutorials/04-Linear-Regression/main.py
+++ tensorflow2.0/tensorflow-2.x-tutorials/04-Linear-Regression/main.py
@@ -3,3 +3,7 @@ import tensorflow as tf
 import numpy as np
 from tensorflow import keras
 import os
+
+class Regressor(keras.layers.Layer):
+    def __init__(self):
+        super(Regressor,self).__init__()
