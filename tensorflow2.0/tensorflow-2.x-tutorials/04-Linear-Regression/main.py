a39232b141502d851fddc9c240436a839c67dbdc
jobsjiang <jht727415@163.com>
2019-10-11 15:42:44 +0800

    lr

--- tensorflow2.0/tensorflow-2.x-tutorials/04-Linear-Regression/main.py
+++ tensorflow2.0/tensorflow-2.x-tutorials/04-Linear-Regression/main.py
@@ -4,6 +4,14 @@ import numpy as np
 from tensorflow import keras
 import os
 
+print(tf.__version__)
+
 class Regressor(keras.layers.Layer):
     def __init__(self):
         super(Regressor,self).__init__()
+        # here must specify shape instead of tensor
+        # name here is meanless
+        # [dim_in,dim_out]
+        self.w = self.add_variable('meanless-name',[13,1])
+        # [dim_out]
+        self.b = self.add_variable('meanless-name',[1])
