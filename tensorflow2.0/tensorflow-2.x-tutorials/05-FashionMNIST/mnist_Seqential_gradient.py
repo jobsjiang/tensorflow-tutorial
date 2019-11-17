6f0fe134b6db43454d77b09a7d63a84feb711f3b
jobsjiang <jht727415@163.com>
2019-11-17 23:08:29 +0800

    fashion

--- tensorflow2.0/tensorflow-2.x-tutorials/05-FashionMNIST/mnist_Seqential_gradient.py
+++ tensorflow2.0/tensorflow-2.x-tutorials/05-FashionMNIST/mnist_Seqential_gradient.py
@@ -3,3 +3,7 @@ import os
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers,optimizers,datasets
+def prepare_mnist_features_labels(x,y):
+    x = tf.cast(x,tf.float32) / 255.0
+    y = tf.cast(y,tf.int64)
+    return x,y
